344a4856eb330aaf431b1828efc28460a94ccb1f
nka21 <133028205+nka21@users.noreply.github.com>
2025-09-15 03:43:41 +0900

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 23:06:35 by nkojima           #+#    #+#             */
+/*   Updated: 2025/08/02 19:52:34 by nkojima          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char const *s2);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+#endif
